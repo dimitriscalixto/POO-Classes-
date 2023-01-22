9403c8b2adecf7343bff2306acb349636dd0bb8e
idimitrisc47 <dimitrisc47@gmail.com>
2023-01-22 13:56:51 -0500

    "Métodos em instâncias de classes Python"

---
+++ inicio.py
@@ -0,0 +1,21 @@
+class Pessoa:
+    def __init__(self, nome, sobrenome, idade):
+        self.nome = nome
+        self.sobrenome = sobrenome
+        self.idade = idade
+
+
+class Carro:
+    def __init__(self, marca, nome, cor, ):
+        self.marca = marca
+        self.nome = nome
+        self.cor = cor
+
+    def acelerar(self):
+        print(f'{self.nome} está acelerando')
+
+
+p1 = Pessoa('Dimitris', 'Calixto', 19)
+c1 = Carro('Fiat', 'Cronos', 'Preto')
+
+c1.acelerar() # Crono está acelerando
